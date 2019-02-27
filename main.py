43ae6e1dc392c84b006f1190e86adb7a422295e4
dawsonbooth <github@dawsonbooth.com>
2019-02-26 19:14:32 -0600

    Remove style

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from utils import MainWindow
 
 if __name__ == '__main__':
     app = QApplication([])
-    # app.setStyle("fusion")  # TODO: windowsxp
     win = MainWindow()
     win.show()
     app.exec_()
