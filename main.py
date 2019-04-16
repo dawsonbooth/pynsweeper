d135dd4f73adda04ef56c328e77a5b0c3c49bde6
dawsonbooth <github@dawsonbooth.com>
2019-04-15 20:45:31 -0500

    Add 'windows' style

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import argparse
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QStyleFactory
 
 from utils import MainWindow
 
 if __name__ == '__main__':
     app = QApplication([])
     win = MainWindow()
+    app.setStyle(QStyleFactory.create('Windows'))
     win.show()
     app.exec_()
